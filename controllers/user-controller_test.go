99f68dbff08cba30c2d9bdbb336dc4e06e68ea28
nicchunglow <iamnicholaschung@gmail.com>
2023-06-14 23:28:25 +0800

    Remove comments

--- controllers/user-controller_test.go
+++ controllers/user-controller_test.go
@@ -17,12 +17,10 @@ func TestGetAllUsers(t *testing.T) {
 	db, mock, _ := sqlmock.New()
 	defer db.Close()
 
-	// Set the mock GORM DB instance in the database package
-	// Convert the SQL database connection to a GORM DB instance
 	gormDB, _ := gorm.Open(postgres.New(postgres.Config{
 		Conn: db,
 	}), &gorm.Config{})
-	// Set the GORM DB instance in the database package
+
 	database.Database.Db = gormDB
 
 	rows := sqlmock.NewRows([]string{"id", "first_name", "last_name"}).
@@ -39,6 +37,4 @@ func TestGetAllUsers(t *testing.T) {
 
 	assert.Nil(t, err)
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-	// Add assertions for the response body if necessary
 }

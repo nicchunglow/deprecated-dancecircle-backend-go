52bab7de113afe5db5147db278e095342409fef5
nicchunglow <iamnicholaschung@gmail.com>
2023-06-30 22:25:43 +0800

    revert test code

--- controllers/user-controller_test.go
+++ controllers/user-controller_test.go
@@ -1,61 +1,56 @@
-package controller_test
+package controller
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gofiber/fiber/v2"
-	controller "github.com/nicchunglow/dancecircle-backend/controllers"
-	"github.com/nicchunglow/dancecircle-backend/models"
-	"github.com/onsi/ginkgo"
-	"github.com/onsi/gomega"
+	"github.com/nicchunglow/dancecircle-backend/database"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-type MockUserRepository struct{}
+func TestGetAllUsers(t *testing.T) {
+	db, mock, _ := sqlmock.New()
+	defer db.Close()
 
-func (m *MockUserRepository) GetAll() ([]models.User, error) {
-	// Mock implementation to return sample users
-	mockUsers := []models.User{
+	gormDB, _ := gorm.Open(postgres.New(postgres.Config{
+		Conn: db,
+	}), &gorm.Config{})
+
+	database.Database.Db = gormDB
+
+	users := []User{
 		{ID: 1, FirstName: "John", LastName: "Doe"},
-		{ID: 2, FirstName: "JohnJohn", LastName: "Doey"},
+		{ID: 2, FirstName: "Jane", LastName: "Smith"},
 	}
-	return mockUsers, nil
+
+	columns := []string{"id", "first_name", "last_name"}
+
+	rows := sqlmock.NewRows(columns)
+	for _, user := range users {
+		rows.AddRow(user.ID, user.FirstName, user.LastName)
+	}
+
+	mock.ExpectQuery(`SELECT \* FROM "users"`).WillReturnRows(rows)
+
+	app := fiber.New()
+	app.Get("/users", GetAllUsers)
+
+	req := httptest.NewRequest(http.MethodGet, "/users", nil)
+	resp, err := app.Test(req)
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	body, err := ioutil.ReadAll(resp.Body)
+
+	expectedBodyBytes, _ := json.Marshal(users)
+	expectedBody := string(expectedBodyBytes)
+
+	assert.Equal(t, expectedBody, string(body))
 }
-
-var _ = ginkgo.Describe("UserController", func() {
-	var (
-		mockUserRepository *MockUserRepository
-		app                *fiber.App
-	)
-
-	ginkgo.BeforeEach(func() {
-		mockUserRepository = &MockUserRepository{}
-		userController := controller.UserController(mockUserRepository)
-		app = fiber.New()
-		app.Get("/users", userController.GetAllUsers)
-	})
-
-	ginkgo.It("should return all users", func() {
-		req := httptest.NewRequest(http.MethodGet, "/users", nil)
-		resp, err := app.Test(req)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		defer resp.Body.Close()
-
-		gomega.Expect(resp.StatusCode).To(gomega.Equal(http.StatusOK))
-
-		var responseUsers []controller.User
-		err = json.NewDecoder(resp.Body).Decode(&responseUsers)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-		gomega.Expect(responseUsers).To(gomega.HaveLen(2))
-
-		gomega.Expect(responseUsers[0].ID).To(gomega.Equal(uint(1)))
-		gomega.Expect(responseUsers[0].FirstName).To(gomega.Equal("John"))
-		gomega.Expect(responseUsers[0].LastName).To(gomega.Equal("Doe"))
-
-		gomega.Expect(responseUsers[1].ID).To(gomega.Equal(uint(2)))
-		gomega.Expect(responseUsers[1].FirstName).To(gomega.Equal("JohnJohn"))
-		gomega.Expect(responseUsers[1].LastName).To(gomega.Equal("Doey"))
-	})
-})
